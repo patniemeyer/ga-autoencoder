19d31ad6d5587aca8b84759de0c8c4cc20fae016
Patrick Niemeyer <pat@pat.net>
2019-01-21 12:38:32 -0600

    WIP

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 
-
 How can I improve the convergence of this genetic algorithm in training an auto encoder on MNIST?
 
-My example code below (only two files / a few hundred lines of python including the GA lib)  trains a three layer auto encoder on MNIST using the GA.
+*autoencoder-ga.py* - A classic GA implementation with selection based on fitness.
 
-https://github.com/patniemeyer/ga-autoencoder
+autoencoder-es.py - An evolutionary strategy that uses a sum of the entire population weighted by fitness (https://arxiv.org/pdf/1703.03864.pdf)
 
-My code appears to be working but compared to back-propagation is incredibly slow.  I keep reading papers and anecdotes about training networks with millions of free parameters and seeing results in just a few generations, however my (comparatively small, 200k parameter) network converges extremely slowly.  With a training set limited to only a hundred images I have seen it continue to make (slow) progress after 50k generations.
+The GA code appears to be working but compared to back-propagation is incredibly slow.  
+The ES code is fast but gets stuck and exhibits weird behavior.  It seems very senstive to the training parameters and amount of data.
 
-Things I’ve tried / learned:
-1. Crossover doesn’t seem to help so I’ve just maxed out mutation.
-2. I have subtracted the mean MNIST image from the input batches, since the algorithm always seems to need to find the mean image (huge local minima) before differentiating between the digits.
-3. I’ve insured that there are always small, single value, single layer mutations available so that it should never gets completely “stuck”.
-4. I’ve tried to make sure that my weight initialization has a distribution similar to what I know to be decent final results.  (But even if I’m wildly off it should just take longer to converge, right?)
-5. I’ve tried experimenting with “safe mutations” basing the mutation magnitude on the gradient of the output with respect to the weights, but I did not see any improvement in simple tests and gave up (perhaps prematurely).  Regardless, I would prefer not to be dependent on being able to calculate the gradients - that’s why I’m using a GA! :)
-6. I have experimented with pytorch’s weight_norm() wrapper that separates the direction and magnitude of the weight tensor and makes them layer parameters: My thinking was that if I’m wildly off in the initialization perhaps this would give the GA more leverage on it.  I really had high hopes that this would help but again, either I’m doing it wrong or there was no improvement.
+Both algorithms exhibit the interesting behavior of starting off with a flat line on the fitness curve before eventually "discovering" something and then beginning the descent.  I would like to better understand what is happening at that inflection point.
 
-I am aware that I can improve the runtime performance of the GA through parallelization and reduce the memory footprint by storing only seed values for mutations, etc.  Right now I’m just trying to hone the algorithm and see some results before spending more time tweaking performance.
-
+I am aware that I can improve the runtime performance of both of these through parallelization and reduce the memory footprint by storing only seed values for mutations, etc.  Right now I’m just trying to hone an algorithm and see some results before spending more time tweaking performance.
 
 
---
+++ autoencoder-es.py
@@ -0,0 +1,156 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from torch import nn
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from itertools import chain
+import numpy as np
+
+if torch.cuda.is_available(): torch.set_default_tensor_type('torch.cuda.FloatTensor')
+# np.random.seed(42)
+if not os.path.exists('./out/'): os.mkdir('./out/')
+# os.system('open ./out');  # exit()
+f = open('loss.txt', 'w'); f.truncate(); f.close()
+
+# e.g. [128,784]->[128,28,28], [-1,1] to [0,1]
+def to_img(x):
+    # x = 0.5 * (x + 1)
+    # x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    # normalize(mean, std): input[channel] = (input[channel] - mean[channel]) / std[channel]
+    # [0-1] -> [-1,1]
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128), nn.ReLU(True),
+            nn.Linear(128, 64), nn.ReLU(True),
+            nn.Linear(64, 12), nn.ReLU(True),
+            nn.Linear(12, 3))
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12), nn.ReLU(True),
+            nn.Linear(12, 64), nn.ReLU(True),
+            nn.Linear(64, 128), nn.ReLU(True),
+            nn.Linear(128, 28 * 28),
+            nn.Tanh()
+        )
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        x = self.encoder(x)
+        # if epoch % 100 == 0: print(epoch, x.numpy())
+        x = self.decoder(x)
+        return x
+
+    # Return the weights and biases
+    def getWeights(self) -> [torch.Tensor]:
+        for module in chain(self.encoder.modules(), self.decoder.modules()):
+            if hasattr(module, 'weight'):
+                yield module.weight.data
+                yield module.bias.data
+
+    # Set weights and biases from the list
+    def setWeights(self, weights: [torch.Tensor]):
+        def chunker(seq, size):
+            return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+        weight_bias_pairs = chunker(weights, 2)
+        for module in chain(self.encoder.modules(), self.decoder.modules()):
+            if hasattr(module, 'weight'):
+                module.weight.data, module.bias.data = next(weight_bias_pairs)
+
+
+def fitness(weights, save=False):
+    # Testing
+    # loss = criterion(weights[0], weight_target[0]) + criterion(weights[1], weight_target[1])
+    # return loss.item()
+
+    model.setWeights(weights)
+    epochloss = 0
+    for batch in traindataloader:
+        img, _ = batch  # [128, 1, 28, 28]
+        img = img.view(img.size(0), -1)  # flat [128, 784]
+        if torch.cuda.is_available(): img = img.cuda()
+        output = model(img)  # [128, 784]
+
+        batch_loss = criterion(output, img).item()  # scalar
+        epochloss += batch_loss
+
+    if save:
+        images = [
+            img[0].view(1, 28, 28), to_img(output[0:1]).squeeze().unsqueeze(0),
+            img[1].view(1, 28, 28), to_img(output[1:2]).squeeze().unsqueeze(0)
+        ]
+        save_image(images, './out/image_{}.png'.format(epoch))
+
+    return epochloss
+
+
+batch_size = 128
+traindataset = MNIST('./data', train=True, transform=img_transform, download=True)
+#traindataset = list(traindataset)[:128]  # limit images
+traindataset = list(traindataset)[:2]  # limit images
+
+# Subtract mean of training data images
+data = torch.stack([d[0] for d in traindataset])  # one big batch [128, 1, 28, 28])
+mean_img = torch.mean(data, 0, keepdim=True)  # batch average image
+traindataset = [(batch - mean_img, clas) for batch, clas in traindataset]
+
+traindataloader = DataLoader(traindataset, batch_size=batch_size, shuffle=False)
+model = autoencoder()
+if torch.cuda.is_available(): model.cuda()
+criterion = nn.MSELoss(reduction='sum')
+weight_start = list(model.getWeights())
+# Testing
+# weight_target = [torch.Tensor([0.5,0.7,0.9]), torch.Tensor([0.1,0.2,0.3])]
+# weight_start = [torch.Tensor([0.1,0.2,0.3]), torch.Tensor([0.3,0.4,0.5])]
+
+# hyperparams
+npop = 25  # population size [25]
+sigma = 1e-3  # noise standard deviation [1e-3]
+alpha = 0.01 # learning rate [0.01]
+
+epoch=0
+def train(num_epochs = 10000, target_fitness=1.0):
+    global epoch
+    # initial weight guess
+    # weight_current = [torch.randn_like(weight) * sigma for weight in weight_start]
+    weight_current = [torch.zeros_like(weight) for weight in weight_start]
+
+    for epoch in range(num_epochs):
+        current_fitness = fitness(weight_current)
+        if current_fitness <= target_fitness: break
+        f=open('loss.txt', 'a'); print(current_fitness, file=f); f.close()
+        if epoch % 1 == 0:
+            print('epoch %d. fitness: %f' % (epoch, current_fitness))
+        if epoch % 100 == 0:
+            fitness(weight_current, save=True)
+
+        R = np.zeros(npop)  # population fitness
+        N = [[]] * npop # population noise
+        for j in range(npop):
+            N[j] = [torch.randn_like(weight) for weight in weight_current]
+            wtry = [a + b * sigma for a, b in zip(weight_current, N[j])]
+            R[j] = fitness(wtry)  # evaluate jittered version
+
+        # standardize the rewards to have a gaussian distribution
+        F = (R - np.mean(R)) / np.std(R)
+
+        # w = w + alpha / (npop * sigma) * np.dot(N.T, A)
+        for i in range(npop):
+            for j in range(len(weight_current)):
+                weight_current[j] = weight_current[j] - alpha / npop * F[i] * N[i][j]
+
+
+train()
+
